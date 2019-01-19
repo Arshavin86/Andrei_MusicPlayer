f537180fbe11413a8ff0a0de111b0bded465b251
Arshavin86 <sevost.andrey@gmail.com>
2019-01-19 14:14:09 -0800

    CRUD is working

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -25,43 +25,43 @@ it('Should return item with given ID and a status of 200', () => {
 })
 
 // POST an item into db:
-// it('POST should return a status of 201 created', () => {
-//     return frisby.post('http://127.0.0.1:8000/api/jane/player/', {
-//         album: "Generator", 
-//         artist: "BadReligion", 
-//         duration: 100,
-//         released: faker.date.past(),
-//         title: 'Punk',
-//         image: 'http://lorempixel.com/640/480/abstract',
-//         song_url: 'https://s3-us-west-1.amazonaws.com/democrituscloud/RASPUTIN_-_Funk_Overload.mp3'
-//     })
-//     .expect('status', 200)
-//     .then((result) => {
-//         console.log('result of POST: ',result);
-//   });
-// })
+it('POST should return a status of 201 created', () => {
+    return frisby.post('http://127.0.0.1:8000/api/jane/player/', {
+        album: "Generator", 
+        artist: "BadReligion", 
+        duration: 100,
+        released: faker.date.past(),
+        title: 'Punk',
+        image: 'http://lorempixel.com/640/480/abstract',
+        song_url: 'https://s3-us-west-1.amazonaws.com/democrituscloud/RASPUTIN_-_Funk_Overload.mp3'
+    })
+    .expect('status', 201)
+    .then((result) => {
+        console.log('result of POST: ',result);
+  });
+})
 
 // // Update an item on Db:
-// it ('Update should return a status of 200 OK', function () {
-//     return frisby
-//     .put(`http://127.0.0.1:8000/api/jane/player/${itemID}`, {
-//         album: "My Generation", 
-//         artist: "GoodReligion", 
-//         duration: 101,
-//         released: faker.date.past(),
-//         title: 'PunkRock',
-//         image: 'http://lorempixel.com/640/480/abstract',
-//         song_url: 'https://s3-us-west-1.amazonaws.com/democrituscloud/RASPUTIN_-_Funk_Overload.mp3'
-//     })
-//     .expect('status', 200)
-//     .then((result) => {
-//         console.log('result from server: ', result.json);
-//     });
-// });
+it ('Update should return a status of 200 OK', function () {
+    return frisby
+    .put(`http://127.0.0.1:8000/api/jane/player/${itemID}`, {
+        album: "My Generation", 
+        artist: "GoodReligion", 
+        duration: 101,
+        released: faker.date.past(),
+        title: 'PunkRock',
+        image: 'http://lorempixel.com/640/480/abstract',
+        song_url: 'https://s3-us-west-1.amazonaws.com/democrituscloud/RASPUTIN_-_Funk_Overload.mp3'
+    })
+    .expect('status', 200)
+    .then((result) => {
+        console.log('result from server: ', result.json);
+    });
+});
 
 //Delete an item on db: 
-// it ('DELETE should return a status of 204 No Content', function () {
-//     return frisby
-//       .del(`http://127.0.0.1:8000/api/jane/player/${itemID}`)
-//       .expect('status', 204);
-//   });
+it ('DELETE should return a status of 204 No Content', function () {
+    return frisby
+      .del(`http://127.0.0.1:8000/api/jane/player/${itemID}`)
+      .expect('status', 204);
+  });
