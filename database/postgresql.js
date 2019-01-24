44fa826a4e8a7ccde93757029c632104113b9808
Arshavin86 <sevost.andrey@gmail.com>
2019-01-23 19:31:35 -0800

    Minor changes

--- database/postgresql.js
+++ database/postgresql.js
@@ -12,16 +12,16 @@ const databaseConfig = {
 };
 
 // Creating a reusable/static ColumnSet for generating INSERT queries:
-const cs = new pgp.helpers.ColumnSet([
-  'id',
-  'album',
-  'artist', 
-  'duration',
-  'released',
-  'title',
-  'image',
-  'song_url',
-], {table: 'songs'});
+// const cs = new pgp.helpers.ColumnSet([
+//   'id',
+//   'album',
+//   'artist', 
+//   'duration',
+//   'released',
+//   'title',
+//   'image',
+//   'song_url',
+// ], {table: 'songs'});
 
 // console.log(playlist);
 
@@ -29,12 +29,12 @@ const db = pgp(databaseConfig);
 
 db.connect()
   .then(obj => {
-    console.log('connected to database');
+    console.log('connected to PostgreSQL');
       obj.done(); // success, release the connection;
   })
   .catch(error => {
       console.log('ERROR:', error.message || error);
 });
 
-module.exports = { db, cs };
+module.exports = { db };
 
